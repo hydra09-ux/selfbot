3882b959baa6df7be031515bdbb0d3fdb700370f
hydra09-ux <go1704090@gmail.com>
2024-10-03 07:16:34 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+ module.exports = {
+ *          token: 'your_discord_bot_token_here',
+ *          prefix: '!',
+ *          userId: 'your_user_id_here'
+ *          // Add any other configuration variables as needed
+ *      };
